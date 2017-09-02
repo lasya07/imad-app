d855ae3c3cffb464e970abc3337918ea0fbea15f
lasya07 <lasya.sam@gmail.com>
2017-09-02 15:11:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,8 +18,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/articles/:articleName',function(req,res){
+    pool.query("SELECT * from article where title='"+req.params.articleName+"'",function(req,res){
+      if(err){
+            res.status(500).send(err.toString());
+            
+        }else{
+            if(result.rows.length===0){
+                res.status(404).send('article not found');
+            }else{
+                var articleData=result.rows[0];
+                 res.send(createTemplate(articleData));
+            }
+        } 
+        
+    });
+   
 });
 app.get('/article-two',function(req,res){
     res.send('Article-two requested and will be served here');
