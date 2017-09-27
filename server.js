2350beec808448f2e7b4c258272139e30b6d4c0e
lasya07 <lasya.sam@gmail.com>
2017-09-27 13:15:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,9 +139,10 @@ app.get('/:articleName',function(req,res){
 });
 
 var counter=0;
+
 app.get('/counter', function (req, res) {
-   counter=counter+1;
-   res.send(counter.toString());
+  counter=counter+1;
+  res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
