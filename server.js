c27029ecc67ef44424d6c7137cd950f21e01404d
lasya07 <lasya.sam@gmail.com>
2017-09-28 08:38:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter', function (req, res) {
+  counter=counter+1;
+  res.send(counter.toString());
+});
+
 var articles={
     'article-one':{
     title:'Article-one|lasya',
@@ -138,12 +144,7 @@ app.get('/:articleName',function(req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-var counter=0;
 
-app.get('/counter', function (req, res) {
-  counter=counter+1;
-  res.send(counter.toString());
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
